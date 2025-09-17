40bc0aab35d46b77a8138c6098d727d379cd2bf1
HappyLoaf <happyloaf@happyloaf.com>
2025-09-17 10:22:12 +0100

    First test post!

---
+++ ih4xeu/_posts/2025-09-17-hello-world.md
@@ -0,0 +1,14 @@
+---
+layout: post
+title:  "Hello World!"
+tag: hello world, introduction, blog
+#category: general
+---
+
+# Hello World!
+
+**Hello world**, this is my first Jekyll blog post.
+
+I hope you like it! Now all I need to do is find something to post about!
+
+
---
+++ ih4xeu/_site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"Hello World! Hello world, this is my first Jekyll blog post. I hope you like it! Now all I need to do is find something to post about! ","categories": [],
+        "tags": ["hello world, introduction, blog"],
+        "url": "http://localhost:4000/blog/2025/09/17/hello-world.html"
+      }]
