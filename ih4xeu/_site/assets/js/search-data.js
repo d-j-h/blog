ca969550987fb3ee02063343c4fd00f37cd7e1ad
HappyLoaf <happyloaf@happyloaf.com>
2025-09-18 08:35:26 +0100

    Re-deployed and updated config to point to root instead of /blog

--- ih4xeu/_site/assets/js/search-data.js
+++ ih4xeu/_site/assets/js/search-data.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Hello World!",
         "excerpt":"Hello World! Hello world, this is my first Jekyll blog post. I hope you like it! Now all I need to do is find something to post about! ","categories": [],
         "tags": ["hello world, introduction, blog"],
-        "url": "http://localhost:4000/blog/2025/09/17/hello-world.html"
+        "url": "http://localhost:4000/2025/09/17/hello-world.html"
       }]
